2a0353bcc20b8e36de62a8231c5214fc321e2b6e
ali <aliyoo7887@gmail.com>
2023-07-25 19:57:12 +0330

    [NEW]: modified tailwind config and add some svg icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      redHat: ["Red Hat Text", "sans-serif"],
+    },
+    colors: {
+      "grayish-blue": "hsl(237, 18%, 59%)",
+      "soft-red": "hsl(345, 95%, 68%)",
+      "dark-desaturated-blue": "hsl(236, 21%, 26%)",
+      "very-dark-blue": "hsl(235, 16%, 14%)",
+      "mostly-black": "hsl(234, 17%, 12%)",
+    },
     extend: {},
   },
   plugins: [],
