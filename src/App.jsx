89bc2722e71727fb7493bbf960ca701364fab9bd
ali <aliyoo7887@gmail.com>
2023-07-26 17:04:23 +0330

    [FIX]: fixed some bugs and imporove app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { Instagram } from "./assets/Instagram";
 
 function App() {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-evenly gap-8'>
-      <section className='flex flex-col items-center justify-center gap-9 bg-stars px-4'>
+    <main className='min-h-screen flex flex-col items-center justify-evenly gap-8 bg-stars'>
+      <section className='flex flex-col items-center justify-center gap-9 px-4'>
         <h1 className='font-redHat font-bold uppercase text-white text-2xl lg:text-5xl tracking-widest text-center'>{`we're launching soon`}</h1>
         <Counter targetDate={24} />
       </section>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,37 +1,63 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Counter = () => {
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [secondes, setSecounds] = useState(0);
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if(secondes > 0)
-  //   }, 1000);
-  // }, [targetDate]);
+  const [days, setDays] = useState("00");
+  const [hours, setHours] = useState("00");
+  const [minutes, setMinutes] = useState("00");
+  const [secondes, setSecounds] = useState("00");
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countDownDate = new Date("July 28, 2023 00:00:00").getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const secondes = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setDays(days);
+        setHours(hours);
+        setMinutes(minutes);
+        setSecounds(secondes);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
   return (
     <div className='flex flex-row gap-4 cursor-default'>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='p-4 lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
           {days}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>days</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='p-4 lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
           {hours}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>hours</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='p-4 lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
           {minutes}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>minutes</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='p-4 lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
           {secondes}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>seconds</p>
