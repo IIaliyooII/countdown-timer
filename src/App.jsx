26553314331d4fc2bf14d023da092802cdc7f3e4
ali <aliyoo7887@gmail.com>
2023-07-27 12:19:28 +0330

    [FIX]: solved some UI and calclulate bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import Counter from "./components/Counter";
 import { Facebook } from "./assets/Facebook";
 import { Pinterest } from "./assets/Pinterest";
 import { Instagram } from "./assets/Instagram";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
   return (
     <main className='min-h-screen flex flex-col items-center justify-evenly gap-8 bg-stars'>
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <input
+          type='datetime-local'
+          name='date'
+          value={date || ""}
+          onChange={(e) => setDate(e.target.value)}
+          className='px-8 py-3 border-none outline-none rounded-md bg-dark-desaturated-blue text-soft-red font-redHat text-xl'
+        />
+        {!isValid && (
+          <span className='font-redHat text-soft-red/60 text-lg'>The date is smaller than the current date</span>
+        )}
+      </div>
       <section className='flex flex-col items-center justify-center gap-9 px-4'>
         <h1 className='font-redHat font-bold uppercase text-white text-2xl lg:text-5xl tracking-widest text-center'>{`we're launching soon`}</h1>
-        <Counter targetDate={24} />
+        <Counter onDate={date} onIsValid={setIsValid} />
       </section>
       <footer className='flex flex-row gap-4 items-center justify-center'>
         <Facebook />
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,65 +1,73 @@
-import { useEffect, useRef, useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 
-const Counter = () => {
-  const [days, setDays] = useState("00");
-  const [hours, setHours] = useState("00");
-  const [minutes, setMinutes] = useState("00");
-  const [secondes, setSecounds] = useState("00");
-  let interval = useRef();
-
-  const startTimer = () => {
-    const countDownDate = new Date("July 28, 2023 00:00:00").getTime();
-    interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const secondes = Math.floor((distance % (1000 * 60)) / 1000);
-
-      if (distance < 0) {
-        clearInterval(interval.current);
-      } else {
-        setDays(days);
-        setHours(hours);
-        setMinutes(minutes);
-        setSecounds(secondes);
-      }
-    }, 1000);
-  };
+const Counter = ({ onDate, onIsValid }) => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [secondes, setSecounds] = useState(0);
 
   useEffect(() => {
-    startTimer();
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const countDownDate = new Date(onDate).getTime();
+
+      if (countDownDate < now) {
+        onIsValid(false);
+        return;
+      }
+
+      const gap = countDownDate - now;
+
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const countDays = Math.floor(gap / day);
+      const countHours = Math.floor((gap % day) / hour);
+      const countMinutes = Math.floor((gap % hour) / minute);
+      const countSeconds = Math.floor((gap % minute) / second);
+
+      if (gap < 100) {
+        clearInterval(interval);
+      } else {
+        onIsValid(true);
+        setDays(countDays);
+        setHours(countHours);
+        setMinutes(countMinutes);
+        setSecounds(countSeconds);
+      }
+    }, 1000);
+
     return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      clearInterval(interval.current);
+      clearInterval(interval);
     };
-  });
+  }, [onDate]);
 
   return (
     <div className='flex flex-row gap-4 cursor-default'>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
-          {days}
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+          {days || "00"}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>days</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
-          {hours}
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+          {hours || "00"}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>hours</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
-          {minutes}
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+          {minutes || "00"}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>minutes</p>
       </div>
       <div className='flex flex-col items-center justify-center gap-3 font-redHat'>
-        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-7xl'>
-          {secondes}
+        <span className='w-16 h-16 lg:w-28 lg:h-28 flex items-center justify-center lg:p-6 bg-dark-desaturated-blue font-bold text-soft-red rounded-md text-3xl lg:text-5xl'>
+          {secondes || "00"}
         </span>
         <p className='uppercase text-xs text-grayish-blue'>seconds</p>
       </div>
